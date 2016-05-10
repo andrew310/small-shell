2b27bbb4e607b6b7ad5c43163c84db46617b7cfc
Andrew Brown <andrewbrown600@gmail.com>
2016-05-10 11:18:04 -0700

    input done

--- smallsh.c
+++ smallsh.c
@@ -7,38 +7,68 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-char** getCommand(int process);
+char* getCommand();
+char** parseCommand(char* command);
 
 int main(int argc, const char * argv[]) {
 
-    char** args;
+    char* args;
+    char** parsedInput = NULL;
+    char* huh = NULL;
     int process = 0;
+    args = getCommand();
+    printf(args); //for testing
+    printf("hi from main"); //for testing
     
-    args = getCommand(process);
+    parsedInput = parseCommand(args);
     
-    printf(args);
-    
-    return 0;
-}
-
-char** getCommand(int process){
-    //string
-    char** args = malloc(sizeof(char*) * 512);
-    char* input = malloc(sizeof(char*) * 2048);
-    int arrSize = 2048;
-    
-    printf(": ");
-    
-    fgets(input, arrSize, stdin);
-    
-    input = strtok(input, "\n");
-    
-    if (input == NULL) {
-        args[0] = NULL;
-        return args;
+    huh = *parsedInput;
+    int i;
+    int arrSize = sizeof(parsedInput);
+    for(i = 0; i <= arrSize; i++){
+        printf(parsedInput[i]);
+        
     }
     
     
+}
+
+char* getCommand(){
+    char* input = malloc(sizeof(char*) * 2048);
+    int arrSize = 2048;
+
+    fgets(input, arrSize, stdin);
+    
     return input;
+}
+
+char** parseCommand(char* command){
+    char** tokens = malloc(sizeof(char*) * 512);
+    char* arg = NULL;
+    
+    //separate the string from the newline character
+    arg = strtok(command, "\n");
+    
+    //if no commands were entered, return
+    if(arg == NULL)
+	{
+		tokens[0] = NULL;
+		return tokens;
+	}
+    
+    int i = 0;
+    //start getting each block of chars delimited by spaces from the input command
+    arg = strtok(arg, " ");
+    //as long as there is still stuff to parse
+	while (arg != NULL) 
+	{
+		tokens[i] = arg;
+		i++;
+		arg = strtok(NULL, " ");
+	}
+    
+    return tokens;
 }
