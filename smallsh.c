dd8851fe9482433f7c1970f2c3917a963627edb8
Andrew Brown <andrewbrown600@gmail.com>
2016-05-11 15:08:25 -0700

    added comments

--- smallsh.c
+++ smallsh.c
@@ -9,6 +9,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 char* getCommand();
 char** parseCommand(char* command);
@@ -43,23 +44,39 @@ int main(int argc, const char * argv[]) {
     
 }
 
+/* Function: execBuiltIn
+ * takes ptr to ptr of array containing parsed input from user
+ * returns an int which will be used to determine if loop continues
+ */
 int execBuiltIn(char** args){
     printf("hello from built in");
     //if user entered a comment
-    if(strcmp(args[0], "#") == 0)
-	{
+    if(strcmp(args[0], "#") == 0){
         	return 1;
     }
     
-    
-    if(strcmp(args[0], "exit") == 0)
-	{
+    if(strcmp(args[0], "exit") == 0){
         	return 0;
     }
- 
+    
+    if(strcmp(args[0], "cd") == 0){
+        if(args[1] == NULL)		{
+    	        	char* path = getenv("HOME");
+    	        	chdir(path);
+    	   	} else
+       		{
+       			char* path = args[1];
+	    		chdir(path);
+       		}
+        	return 1;
+    }
     return 1;
 }
 
+/* Function: getCommand()
+ * takes no parameters, is called from main
+ * returns ptr to an array which will contain raw input from user
+ */
 char* getCommand(){
     char* input = malloc(sizeof(char*) * 2048);
     int arrSize = 2048;
@@ -69,6 +86,10 @@ char* getCommand(){
     return input;
 }
 
+/* Function: parseCommand
+ * takes ptr to array containing raw input from user
+ * returns ptr to parsed input (weeds out " " and \n, returns array commands)
+ */
 char** parseCommand(char* command){
     char** tokens = malloc(sizeof(char*) * 512);
     char* arg = NULL;
