c9ece5082dff976856c0d15554746937ff580682
Andrew Brown <andrewbrown600@gmail.com>
2016-05-14 02:32:47 -0700

    lots of troubleshooting, finally have foreign commands working

--- smallsh.c
+++ smallsh.c
@@ -10,6 +10,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/wait.h>
+#include <termios.h>
 //for opening files
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -20,20 +22,21 @@ char** parseCommand(char* command, int* argsNum);
 int execBuiltIn(char** args, int* exitStatus);
 int execForeign(char** args, int* exitStatus, int* length);
 int arrContainsString(char** arr, char* string, int* length);
+char** trim(char** arr, int* size);
 
 int main(int argc, const char * argv[]) {
 
     int exitStatus = 0;
     int argsNum = 0;
 
-    int shellPrompt = 1;
+    int shellPrompt = 0;
     do{
         char* args;
         char** parsedInput = NULL;
         int process = 0;
+
         args = getCommand();
-        printf(args); //for testing
-        printf("hi from main\n"); //for testing
+
 
         parsedInput = parseCommand(args, &argsNum);
 
@@ -42,79 +45,126 @@ int main(int argc, const char * argv[]) {
         //TODO: check for background run command
         int nativeCommand;
         nativeCommand = execBuiltIn(parsedInput, &exitStatus);
-        if (nativeCommand == 0) {
+        if (nativeCommand == 1) {
             //user chose to exit
-            shellPrompt = 0;
-        }
-        else if (nativeCommand == 1) {
             shellPrompt = 1;
+        }
+        else if (nativeCommand == 0) {
+            shellPrompt = 0;
             //restart loop because we know user entered built in command
             continue;
         } else {
             //note: we shouldn't get here if the user entered a built in command
-            shellPrompt = execForeign(parsedInput, &exitStatus, &argsNum);
+            exitStatus = execForeign(parsedInput, &exitStatus, &argsNum);
         }
 
+        printf("are we there yet: %d\n", shellPrompt);
         //free up memory associated with the input
         free(args);
         free(parsedInput);
 
-    }while(shellPrompt == 1);
+    }while(shellPrompt == 0);
 }
 
 int execForeign(char** args, int* exitStatus, int* length){
     // i/o redirection args
     int outputRedirect = arrContainsString(args, ">", length);
     int inputRedirect = arrContainsString(args, "<", length);
-    int fd;
+    int fd = -1;
     char* file;
+    char** trimmed = NULL;
+    pid_t childPid = -5;
+    struct sigaction act;
+    int exitMethod, childStatus = 0;
 
-    printf("search array results: %d\n", outputRedirect);
+    //printf("search array results: %d\n", outputRedirect);
 
     if (outputRedirect > 0) {
         //add 1 b/c we returned the spot where the > is
         file = args[outputRedirect + 1];
         fd = open(file, O_WRONLY|O_TRUNC|O_CREAT, 0644);
-        printf("opened file: %s\n", args[outputRedirect]);
+        //printf("opened file: %s\n", args[outputRedirect+1]);
     }
     if (inputRedirect > 0) {
         file = args[inputRedirect + 1];
-        fd = open(file, O_WRONLY|O_TRUNC|O_CREAT, 0644);
-    }
-    printf("length of ur array here we go: %d\n", *length);
-    int i;
-    //loop over array and compare to string
-    for(i = 0; i < *length; i++){
-        printf(args[i]);
+        fd = open(file, O_RDONLY, 0644);
     }
 
+    //printf("input redirect: %d\n", inputRedirect);
 
-
-    // else if(((strcmp(args[1], ">") == 0) || (strcmp(args[1], "<") == 0))){
-    //     //make temporary copy of file descriptors for stdin and stdout
-    //     //reference: http://stackoverflow.com/questions/4832603/how-could-i-temporary-redirect-stdout-to-a-file-in-a-c-program
-    //     int bak, new;
-    //     fflush(stdout);
-    //     bak = dup(1);
-    //     //3rd argument should contain the filename
-    //     new = open(args[2], O_WRONLY|O_CREAT|O_TRUNC, 0664);
-    //     //error handling for opening file
-    //     if(new == -1) {
-    //       printf("smallsh: no such file or directory");
-    //       fflush(stdout);
-    //       *exitStatus = 1;
-    //     }
-    //
-    //     dup2(new, 1);
-    //     close(new);
-    //     //code here...
-    //     fflush(stdout);
-    //     dup2(bak, 1);
-    //     close(bak);
+    // printf("length of ur array here we go: %d\n", *length);
+    // int i;
+    // //loop over array and compare to string
+    // for(i = 0; i < *length; i++){
+    //     printf(args[i]);
     // }
 
-    printf("hello from execforeign\n");
-    return 1;
+    //create child process
+    childPid = fork();
+
+    switch (childPid) {
+        //if fork < 0 fork was not successful
+        case -1:
+            exit(1);
+            break;
+        //if fork == 0 child process
+        case 0:
+            //if user selected output redirect, replace standard output with output file
+            //reference: http://www.cs.loyola.edu/~jglenn/702/S2005/Examples/dup2.html
+            //exit if unable to create output file
+            if (outputRedirect > 0 && (dup2(fd, 1) < 0)) {
+                _exit(1);
+            }
+
+            if (inputRedirect && (dup2(fd, 0) < 0)) {
+                printf("smallsh: unable to open specified file: %s\n", file);
+                _exit(1);
+            }
+
+            //user sig_dfl macro to user default, ie do not ignore signial interrupts
+            //this is because we are running in the foreground
+            //ref:http://www.gnu.org/software/libc/manual/html_node/Sigaction-Function-Example.html
+            act.sa_handler = SIG_DFL;
+            sigaction(SIGINT, &act, NULL);
+
+            close(fd);
+
+            trimmed = trim(args, length);
+
+
+            //will execute first argument and takes array of supporting arguments
+            //DOES NOT RETURN
+            if (execvp(args[0], trimmed) == -1) {
+                //if we make it here, execvp failed bc the exec family functions do not return
+                printf("%s: no such file or directory\n", args[0]);
+                //exit with error
+                _exit(1);
+            }
+                break;
+        default: // if fork > 0, this part is executed by parent process
+            close(fd);
+
+            //we want the parent process to ignore signals
+            act.sa_handler = SIG_DFL;
+            act.sa_handler = SIG_IGN;
+            sigaction(SIGINT, &act, NULL);
+
+            //wait for child process to finish up
+            waitpid(childPid, &childStatus, 0);
+
+            exitMethod = WEXITSTATUS(childStatus);
+
+            // if (WIFSIGNALED(status)) {
+            //     int sig = WTERMSIG(status);
+            //     char errMsg[50];
+            //     char si
+            // }
+
+            break;
+    }//end of case switch
+
+    //code here executed by both
+    return exitMethod;
 }
 
 /* Function: execBuiltIn
@@ -125,16 +175,16 @@ int execBuiltIn(char** args, int* exitStatus){
     printf("hello from built in\n");
     //if user entered a comment
     if(strcmp(args[0], "#") == 0){
-        	return 1;
+        	return 0;
     }
     //returns a 0 so the loop in main will stop
     else if(strcmp(args[0], "exit") == 0){
-        	return 0;
+        	return 1;
     }
 
     else if(strcmp(args[0], "status") == 0){
             printf("exit status: %d\n", *exitStatus);
-        	return 1;
+        	return 0;
     }
 
     else if(strcmp(args[0], "cd") == 0){
@@ -145,7 +195,7 @@ int execBuiltIn(char** args, int* exitStatus){
        			char* path = args[1];
 	    		chdir(path);
        		}
-        	return 1;
+        	return 0;
     }
     //if we made it this far, none of the build in commands were typed
     //returning -1 signifies to main that it was not a built in command
@@ -157,11 +207,15 @@ int execBuiltIn(char** args, int* exitStatus){
  * returns ptr to an array which will contain raw input from user
  */
 char* getCommand(){
+    tcflush(0, TCIFLUSH);
+
     char* input = malloc(sizeof(char*) * 2048);
     int arrSize = 2048;
 
+    fflush(stdout);
     printf(": ");
     fgets(input, arrSize, stdin);
+    fflush(stdout);
 
     return input;
 }
@@ -214,4 +268,25 @@ int arrContainsString(char** arr, char* string, int* length){
     //if we made it this far, no match
     //we can return 0 here because the < or > wouldn't be the first arg
     return 0;
+}
+
+/* Function: trim
+ * takes an array and an int for length of that array
+ *  cuts off array where so built in commands are not fed into the foreign exec
+ */
+char** trim(char** arr, int* size){
+    char** trimmed = malloc(sizeof(char*) * 512);
+
+    int i = 0;
+    while (i < *size) {
+        if ((strcmp(arr[i], "<") != 0) && (strcmp(arr[i], ">") != 0) && strcmp(arr[i], "&") != 0) {
+            trimmed[i] = arr[i];
+            printf("I BE TRIMMIN: %s\n", trimmed[i]);
+            i++;
+            continue;
+        }
+        //outside of loop we have hit one of those characters, so stop the array hence trimming it
+        break;
+    }
+    return trimmed;
 }
